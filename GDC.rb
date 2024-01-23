85aa67bcf31b7f96ff563d9bef771829e7512111
James Skon <skonjp@gmail.com>
2024-01-23 15:18:55 -0500

    Full program

--- GDC.rb
+++ GDC.rb
@@ -1,1 +1,17 @@
-puts "Hello"
+
+
+def gcd(a, b)
+    while a != b
+        if a > b
+            a = a - b
+        else
+            b = b - a
+        end  
+    end
+    return a 
+end
+    
+
+puts(gcd(10,30))
+puts(gcd(100,25))
+puts(gcd(17,19))
